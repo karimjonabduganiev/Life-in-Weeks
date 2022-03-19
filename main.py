4cf01e77695247f018814cd46443a76406c78d35
karimjonabduganiev <karimjonabduganiyev@gmail.com>
2022-03-19 15:21:31 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+age = int(input("What is your age? "))
+total = 90 - age
+x = 365 * total
+y = 52 * total
+z = 12 * total
+
+final = (f"You have {x} days, {y} weeks, and {z} months left.")
+print(final)
